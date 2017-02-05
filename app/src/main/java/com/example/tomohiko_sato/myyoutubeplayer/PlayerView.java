7d45a73c8cfad296c33a244bbcfa8ea1828bcce5
tomohiko sato <tomohikosato1@gmail.com>
2017-02-05 20:59:10 +0900

    途中

--- app/src/main/java/com/example/tomohiko_sato/myyoutubeplayer/PlayerView.java
+++ app/src/main/java/com/example/tomohiko_sato/myyoutubeplayer/PlayerView.java
@@ -3,7 +3,6 @@ package com.example.tomohiko_sato.myyoutubeplayer;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 
@@ -15,6 +14,12 @@ import android.widget.FrameLayout;
  */
 public class PlayerView extends FrameLayout {
     private final WindowManager windowManager;
+    private State currentState = State.FLOAT;
+
+    enum State {
+        FLOAT,
+        EXPAND
+    }
 
     public PlayerView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,12 +28,15 @@ public class PlayerView extends FrameLayout {
             @Override
             public void onMoved(int dx, int dy) {
                 Logger.d("dx:%d, dy:%d", dx, dy);
-                updateLayout(dx, dy);
+                if (currentState == State.FLOAT) {
+                    updateLayout(dx, dy);
+                }
             }
         }, new TouchEventTranslater.OnClickedListener() {
             @Override
             public void onClicked() {
                 Logger.d();
+                currentState = currentState == State.EXPAND ? State.FLOAT : State.EXPAND;
             }
         }));
     }
@@ -48,56 +56,4 @@ public class PlayerView extends FrameLayout {
         windowManager.updateViewLayout(this, lp);
     }
 
-    /**
-     * タッチイベントを解釈し、{@link OnMovedListener}や{@link OnClickedListener} に伝える
-     */
-    private static class TouchEventTranslater implements OnTouchListener {
-        private final OnMovedListener moved;
-        private final OnClickedListener clicked;
-
-        private int downX = 0;
-        private int downY = 0;
-        private int oldX = 0;
-        private int oldY = 0;
-
-        TouchEventTranslater(OnMovedListener moved, OnClickedListener clicked) {
-            this.moved = moved;
-            this.clicked = clicked;
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    downX = (int) event.getRawX();
-                    downY = (int) event.getRawY();
-                    oldX = downX;
-                    oldY = downY;
-                    break;
-                case MotionEvent.ACTION_UP:
-                    int distance = (int) Math.sqrt(((event.getRawX() - downX) * (event.getRawX() - downX) + (event.getRawY() - downY) * (event.getRawY() - downY)));
-                    if (distance < 30) {
-                        clicked.onClicked();
-                    }
-                    break;
-                default:
-                    int dx = (int) (event.getRawX() - oldX);
-                    int dy = (int) (event.getRawY() - oldY);
-                    oldX = (int) event.getRawX();
-                    oldY = (int) event.getRawY();
-                    moved.onMoved(dx, dy);
-                    break;
-            }
-
-            return false;
-        }
-
-        interface OnMovedListener {
-            void onMoved(int dx, int dy);
-        }
-
-        interface OnClickedListener {
-            void onClicked();
-        }
-    }
 }
